b5bfe8aa40ab7bd51ef5b8bbd44fb8a2e89a0b7d
Osaite Arigbe-Osula <ik.arigbe.osula@gmail.com>
2023-08-11 15:33:14 -0700

    all mandatory tasks

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: pointer to linked list
+ *
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *first, *rev, *tmp1 = NULL, *tmp2 = NULL;
+
+	if (!*head || !(*head)->next)
+		return (1);
+	first = *head;
+	rev = *head;
+	while (rev)
+	{
+		tmp1 = rev->next;
+		rev->next = tmp2;
+		tmp2 = rev;
+		rev = tmp1;
+	}
+	rev = tmp2;
+	while (first && rev)
+	{
+		if (first->n != rev->n)
+			return (0);
+		first = first->next;
+		rev = rev->next;
+	}
+	return (1);
+}
