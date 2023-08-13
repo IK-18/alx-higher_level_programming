dc2e43c484624e224c080ccd82a8bd83764f3c99
Osaite Arigbe-Osula <ik.arigbe.osula@gmail.com>
2023-08-13 13:39:30 -0700

    fix task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,5 +3,6 @@ def new_in_list(my_list, idx, element):
     tmp = my_list.copy()
     if idx == 0 or idx >= len(my_list):
         return tmp
-    tmp[idx] = element
-    return tmp
+    else:
+        tmp[idx] = element
+        return tmp
