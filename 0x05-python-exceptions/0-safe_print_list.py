ec540a3569d565592cf37758034bd68d6b4ebdeb
Osaite Arigbe-Osula <ik.arigbe.osula@gmail.com>
2023-08-28 07:58:49 -0700

    fixes

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        j = 0
-        for i in range(x):
+    j = 0
+    for i in range(x):
+        try:
             print("{}".format(my_list[i]), end="")
             j += 1
-    except(IndexError):
-        pass
+        except IndexError:
+            break
     print()
     return j
--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import sys
 def safe_function(fct, *args):
-    res = None
     try:
         res = fct(*args)
-        return res
     except Exception as err:
         print("Exception: {}".format(err), file=sys.stderr)
+        return None
+    else:
         return res
