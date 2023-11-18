0bdbbb6e7f9d4f6a5bacce585279e5a19a1241c7
Osaite Arigbe-Osula <ik.arigbe.osula@gmail.com>
2023-11-18 15:04:04 -0800

    all tasks

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""lists all State objects from the database hbtn_0e_6_usa"""
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).order_by(State.id):
+        print(state.id, state.name, sep=": ")
+        for city in state.cities:
+            print("\t{}: {}".format(city.id, city.name))
+    session.commit()
---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""class definition of a City and an instance Base = declarative_base()"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from relationship_state import Base
+
+
+class City(Base):
+    """Cities ORM table object
+
+    Attributes:
+        id(int): unique identifier
+        name(str): city
+        state_id(int): foreign key to states.id
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
